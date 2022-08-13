47ae6d155a2b77ab21368633f88446d54c121160
t.gazzaev <t.gazzaev@sftpro.ru>
2022-08-14 00:20:54 +0300

    test pre-commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from "@nestjs/common"
 
-import { AppService } from "./app.service";
+import { AppService } from "./app.service"
 
 @Controller()
 export class AppController {
@@ -8,8 +8,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    const a = 1
-
     return this.appService.getHello()
   }
 }
