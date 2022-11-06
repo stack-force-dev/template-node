a24ae3ba333a2e5cfda10a8f3afb3b94486394b0
t.gazzaev <t.gazzaev@sftpro.ru>
2022-11-06 22:31:13 +0300

    Users base crud

--- src/api/users/users.controller.ts
+++ src/api/users/users.controller.ts
@@ -8,4 +8,29 @@ export class UsersController {
   getUsers() {
     return this.userService.findUsers()
   }
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.usersService.create(createUserDto);
+  // }
+  //
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
+  //
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+  //
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+  //
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
 }
